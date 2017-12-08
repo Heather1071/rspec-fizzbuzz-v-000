a94e6f845b3c3086b6e968454655ef16191e36ac
Heather Arnold <harnold4882@gmail.com>
2017-12-08 18:39:52 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,17 @@
+def fizzbuzz(int)
+  if (int.to_i % 3) == 0 && (int.to_i % 5) ! = 0
+  puts  "Fizz"
+
+elsif 
+  (int. (int.to_i % 3) == 0 && (int.to_i % 5) == 0
+  puts  "FizzBuzz"
+
+elsif
+   (int.to_i % 5) == 0 && (int.to_i % 3) ! == 0
+    puts "Buzz"
+
+else 
+  puts "nil"
+
+end
+end
